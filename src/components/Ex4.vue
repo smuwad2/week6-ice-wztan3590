b9eed79570caacb309bc03fc4e53fa0617264d3c
wztan3590 <weizhi.tan.2024@computing.smu.edu.sg>
2025-09-24 03:12:24 +0000

    commit

--- src/components/Ex4.vue
+++ src/components/Ex4.vue
@@ -2,11 +2,20 @@
     export default { 
         data() {
             return {
-                // Add code here
+                id: "demo1",
+                blueBoth: true
             }
         },
         methods: { 
-   			// Add code here
+   			changeColor() {
+                this.blueBoth = !this.blueBoth
+
+                // if(this.blueBoth != true) {
+                //     this.blueBoth = true
+                // } else {
+                //     this.blueBoth = false
+                // }
+            }
 		}
     }
 </script>
@@ -15,11 +24,14 @@
     
     <!-- Modify code here -->
     <div class="container">
-        <div v-bind:id="id" class="m-2">
+        <div v-bind:id="id" class="m-2" v-bind:class="{'blueBox' : blueBoth, 'redBox' : !blueBoth}">
             div ID : {{id}} 
         </div>
         
-        <button type="button">Change Color</button>
+        <!-- btn-danger & btn-primary -->
+        <button type="button" v-bind:class="{'btn btn-primary' : blueBoth, 'btn btn-danger' : !blueBoth}" 
+        @click="changeColor()">Change Color</button>
+        
     </div>
 
 </template>
