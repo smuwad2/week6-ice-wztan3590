ea0a7764aec4b415ad8f15da77c3bfe048afa9e1
wztan3590 <weizhi.tan.2024@computing.smu.edu.sg>
2025-09-24 01:50:25 +0000

    commit

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -1,7 +1,15 @@
 <script>
     export default { 
         // Add Code Here
-        // challenge: use array  
+        data() {
+            return {
+                message: "Choose your favorite fruit:",
+                fruit1: "apple",
+                fruit2: "orange",
+                fruit3: "peach",
+                fruits: ["apple", "orange", "peach"]
+            }
+        }
     }
 </script>
 
@@ -11,6 +19,13 @@
         {{ fruit1 }} <input type="radio" name="fruit">
         {{ fruit2 }} <input type="radio" name="fruit">
         {{ fruit3 }} <input type="radio" name="fruit">
+
+        <h3>Using Array</h3>
+        {{ message }} </br>
+        <span v-for="fruit in fruits">
+            {{fruit}} <input type="radio" name="fruit">
+        </span>
+
     </div>
 </template>
 
